b94676fd071db5a7eb2084dc7cf73a63c6653cc8
jmfdiazAL <josemanuel.fernandez@iesalandalus.org>
2023-03-06 14:22:37 +0100

    Alquileres, Vehiculos y Clientes pasa los test

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -4,12 +4,14 @@ import javax.naming.OperationNotSupportedException;
 
 import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.ModeloCascada;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.memoria.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 
 public class MainApp {
 
 	public static void main(String[] args) throws OperationNotSupportedException {
-		Modelo modelo = new Modelo();
+		Modelo modelo = new ModeloCascada(FactoriaFuenteDatos.MEMORIA.crear());
 		Vista vista = new Vista();
 		Controlador controlador = new Controlador(modelo, vista);
 		controlador.comenzar();
